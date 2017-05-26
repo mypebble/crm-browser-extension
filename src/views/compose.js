c0a3b5b0b0109600bb6f83d6de5914b90e2eed5e
Joe Simpson <headbangerkenny@gmail.com>
2017-05-26 11:45:45 +0100

    Fixes, set BCC

--- src/views/compose.js
+++ src/views/compose.js
@@ -3,12 +3,21 @@ import {blacklist, BaseCollection, EmptyView} from 'utils';
 import $ from 'jquery';
 
 const EntityView = Mn.View.extend({
-    template: require('templates/compose_entity.jst')
+    template: require('templates/compose_entity.jst'),
+    ui: {
+        'addButton': '.btn-add'
+    },
+    triggers: {
+        'click @ui.addButton': 'add'
+    }
 });
 
 const EntityListView = Mn.CollectionView.extend({
     childView: EntityView,
-    emptyView: EmptyView
+    emptyView: EmptyView,
+    onChildviewAdd: function(child){
+        this.trigger('add', child.model);
+    }
 })
 
 export default Mn.View.extend({
@@ -18,20 +27,29 @@ export default Mn.View.extend({
     },
 
     onRender: function(){
-        this.collection = new BaseCollection();
-        this.showChildView('organisations', new EntityListView({
-            collection: this.collection
-        }));
-
         this.composeView = this.getOption('composeView');
+
+        this.collection = new BaseCollection();
+        const entityListView = new EntityListView({
+            collection: this.collection
+        });
+        this.showChildView('organisations', entityListView);
+        this.listenTo(entityListView, 'add', function(model){
+            // TODO: Make this work properly
+            this.composeView.setBccRecipients(['magni+' + model.get('id') + '@talktopebble.co.uk']);
+        });
 
         console.log(this.composeView.isInlineReplyForm());
         if(this.composeView.isInlineReplyForm() && document.location.hostname == "inbox.google.com"){
             return; // TODO: Handle this better
         }
 
-        this.listenTo(this.composeView, 'toContactAdded', this.refreshCollection);
-        this.listenTo(this.composeView, 'toContactRemoved', this.refreshCollection);
+        this.composeView.on('toContactAdded', () => {
+            this.refreshCollection();
+        })
+        this.composeView.on('toContactRemoved', () => {
+            this.refreshCollection();
+        })
         this.refreshCollection();
     },
 
